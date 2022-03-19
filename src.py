93ed7dc1b260d92fb627d6facbb2efc87e8974df
helix <68465860+HelixDev@users.noreply.github.com>
2022-03-19 09:44:35 +0100

    i got less lazy

--- src.py
+++ src.py
@@ -1,22 +1,22 @@
-def return_array(filename): # im too lazy to change this
+def return_data(filename): # im too lazy to change this
     with open(f"./data/{filename}", 'r') as f:
         import random
         return random.choice([line.split() for line in f.readlines()][0])
 
 class text:
     def single():
-        return return_array("words")
+        return return_data("words")
     
     def phrase(words):
-        return "".join(f"{return_array('words')} " for i in range(words)).lstrip(" ")
+        return "".join(f"{return_data('words')} " for i in range(words)).lstrip(" ")
 
 class personal:
     class name:
         def first():
-            return return_array('first_names')
+            return return_data('first_names')
         
         def last():
-            return return_array('last_names')
+            return return_data('last_names')
         
         def full():
             return f"{personal.name.first()} {personal.name.last()}"
